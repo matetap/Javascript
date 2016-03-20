73f8e6bf92d7a5126a3d41b9e85baa84f7a60592
patoninho1 <zobiri_s@esme.fr>
2016-03-20 08:32:48 -0700

    Some test with electon for node_js and atom as editor

--- main.js
+++ main.js
@@ -1,14 +1,42 @@
+//Dependance
+'use strict';
 var Promise = require("bluebird");
 var Promise = require("grunt");
 var Promise = require("winston");
-var http = require("http");
+const electron = require('electron');
 
-function onRequest(request, response) {
-  console.log("Requ�te re�ue.");
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
+console.log("Debut du programme");
+
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+function createWindow () {
+
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+  //mainWindow.loadURL('file://' + __dirname + '/index.html');
+  //mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+
 }
 
-http.createServer(onRequest).listen(8888);
-console.log("D�marrage du serveur.");
+//appelle createWindow lorsque la fenetre est prette
+app.on('ready', createWindow);
+
+//s'execute quand on ferme la fenetre
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+//Pour garder la fenetre vivante
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
