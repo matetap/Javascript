e0faffbbda0ca0af220326d865e292b5313572f4
patoninho1 <zobiri_s@esme.fr>
2016-03-20 10:42:29 -0700

    Add somes classes

--- main.js
+++ main.js
@@ -9,6 +9,120 @@ var chalk = require('chalk');
 
 console.log(chalk.blue("Debut du programme"));
 
+
+//Horloge qui appel une focntion a chaque heure
+var globalTime = 0;
+var timer = setInterval(
+  function updateTime(){
+    console.log(chalk.blue("tic",globalTime));
+    if (globalTime < 23){
+      globalTime++;
+    }else{
+      globalTime=0;
+    }
+  }
+,1000);
+
+
+var marche =(function(op,cl){
+
+  marche.isopen = function(){
+    if(globalTime>op && globalTime < cl){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+})
+
+
+
+
+var restaurant =(function(amOp,amCl,pmOp,pmCl){
+
+  var patate = 0;
+  var steak = 0;
+  var carote = 0;
+  var salade = 0;
+
+  restaurant.getFood = function(){
+    if (marche.isopen()){
+      patate = 10;
+      steak = 10;
+      carote = 10;
+      salade = 10;
+      var end = globalTime + 5;
+      while (globalTime < end);
+    }
+  }
+
+  restaurant.isopen = function(){
+    if((globalTime>amOp && globalTime < amCl)||(globalTime>pmOp && globalTime < pmCl) ){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  restaurant.makeMesandwitch = function(){
+    if (true) {
+      var end = globalTime + 5;
+      while (globalTime < end);
+      return true
+    }else{
+      return false
+    }
+  }
+
+
+})
+
+
+var client =(function(amOp,amCl,pmOp,pmCl){
+
+
+
+})
+
+
+var Rungis = new marche(11,15,18,23);
+var Resto = new restaurant(11,15,18,23);
+var Cli = new client;
+
+
+
+
+
+
+/*
+const msForOneHour = 1000;
+const timings = {
+  hour : msForOneHour ,
+  chefMin : 0.05 * msForOneHour ,
+  chefMax : 0.5 * msForOneHour ,
+  // etc.
+};
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
@@ -53,8 +167,3 @@ app.on('activate', function () {
 //appelle createWindow lorsque la fenetre est prette
 app.on('ready', createWindow);
 */
-//Test timer
-var timer = setInterval(
-function tic(){
-console.log(chalk.blue("tic"));
-},1000);
