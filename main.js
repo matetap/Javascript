1bf93f2bb8305e456ddd544d4bdc044fc99e61a4
patoninho1 <zobiri_s@esme.fr>
2016-03-20 08:44:46 -0700

    Electon open a html file in window

--- main.js
+++ main.js
@@ -11,14 +11,16 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
 let mainWindow;
-
 function createWindow () {
 
   mainWindow = new BrowserWindow({width: 800, height: 600});
-  //mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
   //mainWindow.webContents.openDevTools();
 
+
+  //a la fermeture de la fenetre
   mainWindow.on('closed', function() {
+    console.log("windows closed");
     mainWindow = null;
   });
 
@@ -27,8 +29,9 @@ function createWindow () {
 //appelle createWindow lorsque la fenetre est prette
 app.on('ready', createWindow);
 
-//s'execute quand on ferme la fenetre
+//s'execute quand on ferme le'app'
 app.on('window-all-closed', function () {
+  console.log("App closed");
   if (process.platform !== 'darwin') {
     app.quit();
   }
@@ -36,6 +39,7 @@ app.on('window-all-closed', function () {
 
 //Pour garder la fenetre vivante
 app.on('activate', function () {
+  console.log("windows is alive !");
   if (mainWindow === null) {
     createWindow();
   }
