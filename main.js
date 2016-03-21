91d0661eea9a7e6bd13ce7810f02dae920f8d9ff
patoninho1 <zobiri_s@esme.fr>
2016-03-21 13:47:50 -0700

    rename some file

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function createWindow () {
 
   console.log(chalk.green("window started"));
   mainWindow = new BrowserWindow({width: 800, height: 600});
-  mainWindow.loadURL('file://' + __dirname + '/parameter.html');
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
   mainWindow.webContents.openDevTools();
 
 
